29d05563efafc42e46f344235a523583b0648cc2
Valdo Ghafoor <valdoghafoor@gmail.com>
2019-05-02 16:46:57 +0200

    Projects overlay

--- src/ui/components/Projects.vue
+++ src/ui/components/Projects.vue
@@ -6,26 +6,31 @@ export default {
       projects: [
         {
           image: require('@/assets/images/portfolios/ly.png'),
+          thumbnail: require('@/assets/images/thumbnails/ly.png'),
           logo: require('@/assets/images/logos/ly.png'),
           name: 'Luxurynsight'
         },
         {
           image: require('@/assets/images/portfolios/agorize.png'),
+          thumbnail: require('@/assets/images/thumbnails/agorize.png'),
           logo: require('@/assets/images/logos/agorize.jpg'),
           name: 'Agorize'
         },
         {
           image: require('@/assets/images/portfolios/bm.png'),
+          thumbnail: require('@/assets/images/thumbnails/bm.png'),
           logo: require('@/assets/images/logos/bm.png'),
           name: 'BackMarket'
         },
         {
           image: require('@/assets/images/portfolios/bof.png'),
+          thumbnail: require('@/assets/images/thumbnails/bof.png'),
           logo: require('@/assets/images/logos/bof.png'),
           name: 'Bank of Fashion'
         },
         {
           image: require('@/assets/images/portfolios/frk.png'),
+          thumbnail: require('@/assets/images/thumbnails/frk.png'),
           logo: require('@/assets/images/logos/frk.png'),
           name: 'FreelanceRepublik'
         }
@@ -39,11 +44,16 @@ export default {
   <div :class="$style['Root']">
     <div :class="$style['List']">
       <template v-for="(project, index) in projects">
-        <img
-          :class="$style['Portfolio']"
-          :src="project.image"
-          :key="`portfolio-${index}`"
+        <div
+          :class="$style['Thumbnail']"
+          :key="`thumbnail-${index}`"
+          :data-name="project.name"
         >
+          <img
+            :class="$style['ThumbnailImage']"
+            :src="project.thumbnail"
+          >
+        </div>
 
         <img
           :src="project.logo"
@@ -74,14 +84,14 @@ export default {
     .List
       display: grid
       align-content: space-around
-      grid-template-columns: repeat(auto-fill, minmax(15rem, 1fr))
+      grid-template-columns: 1fr 1fr
       grid-gap: 1rem
       width: 100%
       height: 100%
       padding: 0
 
       .Logo,
-      .Portfolio
+      .Thumbnail
         margin: 0 auto
         height: auto
         width: 100%
@@ -100,30 +110,44 @@ export default {
         +md
           display: none
 
-      .Portfolio
+      .Thumbnail
         width: 100%
-        height: auto
         box-shadow: 0 .5rem 1rem rgba(0,0,0,0.19), 0 .2rem .2rem rgba(0,0,0,0.23)
         display: none
         margin: 0 auto
         cursor: pointer
         transition: box-shadow .3s ease-out
+        position: relative
 
-        &:hover
-          box-shadow: 0 2rem 3rem rgba(0,0,0,0.19), 0 .8rem .8rem rgba(0,0,0,0.23)
+        .ThumbnailImage
+          width: 100%
+          height: auto
+
+        &::after
+          content: attr(data-name)
+          position: absolute
+          top: 0
+          left: 0
+          width: 100%
+          height: 100%
+          background: rgba(0, 0, 0, 0.8)
+          display: flex
+          align-items: center
+          justify-content: center
+          z-index: 1
+          opacity: 0
+          transition: opacity 0.4s ease-out
+          color: $white
+          font-weight: bold
+
+        &:hover::after
+          opacity: 1
 
         +md
           display: block
 
       +md
-        grid-template-columns: repeat(auto-fill, minmax(30rem, 1fr))
-        grid-gap: 4rem
+        grid-gap: 3rem
         align-content: initial
-
-      +lg
-        grid-template-columns: repeat(auto-fill, minmax(30rem, 1fr))
-
-      +xl
-        grid-template-columns: repeat(auto-fill, minmax(35rem, 1fr))
 
 </style>
